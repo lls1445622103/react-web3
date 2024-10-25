d966164ac93c057447ab12f2090c3490dfc53ac7
lixiaoming <xiaoming.li@cy-tech.net>
2024-10-25 17:17:32 +0800

    压缩文件

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
     implementation: 'sass-embedded',
+  },
+  webpack: (config) => {
+    config.cache = {
+      type: 'filesystem',
+      compression: 'gzip', // 使用 gzip 压缩缓存文件
+      buildDependencies: {
+        config: [__filename],
+      },
+    };
+    return config;
   },
 };
 
